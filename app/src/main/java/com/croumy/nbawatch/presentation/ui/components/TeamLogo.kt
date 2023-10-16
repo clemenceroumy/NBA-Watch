5e9e0c3feebfb4903695653ad8e51069b6a9deaf
croumy <clemence@playmoweb.com>
2023-10-16 17:11:22 +0200

    fix: team logo

--- app/src/main/java/com/croumy/nbawatch/presentation/ui/components/TeamLogo.kt
+++ app/src/main/java/com/croumy/nbawatch/presentation/ui/components/TeamLogo.kt
@@ -1,27 +1,45 @@
 package com.croumy.nbawatch.presentation.ui.components
 
 import Team
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.croumy.nbawatch.presentation.theme.Dimensions
 
 @Composable
 fun TeamLogo(team: Team, size: Dp = Dimensions.sIcon) {
     val context = LocalContext.current
+    val picture = remember { mutableStateOf<Int?>(null) }
+
+    LaunchedEffect(team.teamTricode) {
+        val identifier = context.resources.getIdentifier(
+            team.teamTricode.lowercase(),
+            "drawable",
+            context.packageName
+        )
+
+        picture.value = if(identifier != 0) identifier else null
+    }
+
     Image(
         painter = painterResource(
-            id = context.resources.getIdentifier(
-                team.teamTricode.lowercase(),
+            id = picture.value ?: context.resources.getIdentifier(
+                "placeholder",
                 "drawable",
-                context.packageName
+                context.packageName,
             )
         ),
         contentDescription = "",
-        modifier = Modifier.size(size)
+        modifier = Modifier.size(size).padding(if(picture.value == null) Dimensions.xxsPadding else 0.dp)
     )
 }
